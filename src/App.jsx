d507db138bdbbfb0271fe0d2fbbc89303d63a170
MostakemHossain <mostakememon123@gmail.com>
2024-11-23 16:23:26 +0600

    About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 
@@ -5,7 +6,7 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="bg-blue-500 z-0 min-h-screen"></section>
+      <About />
     </main>
   )
 }
